91522997695b262bbcad73dfe1fa3e3585a77449
Othmane Sami <othmane.sami@finamaze.com>
2020-10-24 09:05:30 +0100

    created new transaction

--- blockchain.py
+++ blockchain.py
@@ -10,8 +10,23 @@ class Blockchain(object):
     def new_block(self):
         pass
 
-    def new_transaction(self):
-        pass
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go to the next mined block
+        :param sender: <str> address of the sender
+        :param recipient: <str> address of the recipient
+        :param amount: <int> Amount
+        :return: <int> the index of the block that will hold this transaction
+        """
+
+        self.current_transactions.append({
+            'sender' : sender,
+            'recipient' : recipient,
+            'amount' : amount
+        })
+
+        return self.last_block['index'] + 1
+
 
     @staticmethod
     def hash(block):
